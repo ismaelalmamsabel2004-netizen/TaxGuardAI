578c6e3c634d5dabd65b519ef5fe6b8807bdfc8b
ismaelalmamsabel2004-netizen <ismaelalmamsabel2004@gmail.com>
2026-07-26 12:34:21 +0200

    Actualizacion

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { ClerkProvider } from '@clerk/nextjs';
 import type { Metadata, Viewport } from 'next'
 import { Inter } from 'next/font/google'
@@ -37,6 +38,7 @@ export default function RootLayout({
           <PwaActivator />
           {children}
           <Analytics />
+          <SpeedInsights />
         </body>
       </ClerkProvider>
     </html>
